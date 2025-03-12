2ee1bc2ec70796027ac934be65a85733e2ea317c
Kervin Gonzalez <kervingonzalez@gmail.com>
2025-03-11 22:59:28 -0400

    add interfaces

---
+++ src/app/pages/tutors/interfaces/tutor.interface.ts
@@ -0,0 +1,11 @@
+export interface Tutor {
+  id: number;
+  first_name: string;
+  last_name: string;
+  birth_date: Date;
+  email: string;
+  phone: string;
+  role_id: number;
+  status: string;
+  speciality: string;
+}
