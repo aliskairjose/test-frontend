e870405784401e3b2f0971209d32fee6fb9acf64
Kervin Gonzalez <kervingonzalez@gmail.com>
2025-03-12 00:01:20 -0400

    change colors

--- src/app/pages/tutors/constants/speciality.constant.ts
+++ src/app/pages/tutors/constants/speciality.constant.ts
@@ -1,6 +1,6 @@
 export const SpecialityType: Record<string, any> = {
-  Begginer: 'success',
-  Fluent: 'primary',
-  Intermediate: 'warning',
-  Advanced: 'danger',
+  Beginner: 'danger',
+  Fluent: 'warning',
+  Intermediate: 'primary',
+  Advanced: 'success',
 };
