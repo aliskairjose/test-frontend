628b235d6df917b63de4cef4fba2f7634ab348e6
Kervin Gonzalez <kervingonzalez@gmail.com>
2025-03-11 21:26:27 -0400

    add nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -7,5 +6,11 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class AppComponent {
+  public appPages = [
+    { title: 'Usuarios', url: '/home', icon: 'people' },
+    { title: 'Tutores', url: '/tutors', icon: 'people-circle' },
+    { title: 'Reservas', url: '/student', icon: 'bookmarks' },
+    { title: 'Clases', url: '/classes', icon: 'book' },
+  ];
   constructor() {}
 }
